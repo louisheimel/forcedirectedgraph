8e92bb112ed8820121e348f1aa8521c88e5fe8b0
Louis Heimel <louis.heimel@mines.sdsmt.edu>
2017-07-19 10:20:06 -0500

    move flags

--- public/script.js
+++ public/script.js
@@ -5,7 +5,7 @@ var config = {
 	height: 700,
   leftFlagOffset: 550,
   topFlagOffset: 375,
-  leftLinkOffset: -110,
+  leftLinkOffset: -105,
   topLinkOffset: -45,
   margin: 'auto',
   display: 'block',
